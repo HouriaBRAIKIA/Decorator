16b5190df87baa89dcce26bf928e13116437c931
Houria BRAIKIA <83876346+HouriaBRAIKIA@users.noreply.github.com>
2023-11-23 18:18:13 +0100

    ajouter un autre objet node

--- Deco/src/deco/Main.java
+++ Deco/src/deco/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // Exemple d'utilisation
         Event myNode = new Node("I am DEVIL");
+        Event myNode1 = new Node("I am DEVIL");
 
         Event nodeWithSound = new SoundNodeDecorator(myNode, "audio.wav");
         nodeWithSound.display();
